441502da53d90cd7667cf29399ec470c153309de
a5677a <a5677a@naver.com>
2021-08-12 14:34:31 +0900

    중간점검1

--- humanandbook/settings.py
+++ humanandbook/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =
+SECRET_KEY = 'django-insecure-#3fr23yyogky542ypz86x8s#*zjqf+49hao1$@4iwf0recxs&w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
