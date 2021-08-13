7b790a10b7f9892aff053ecce242d6acf2b5120d
yunzzu <whdsbswn@gmail.com>
2021-08-13 17:29:51 +0900

    docker secret

---
+++ humanandbook/settings/deploy.py
@@ -0,0 +1,43 @@
+
+from .base import *
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# reading .env file
+environ.Env.read_env(
+    env_file = os.path.join(BASE_DIR, '.env')
+)
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+### Raises django's ImproperlyConfigured exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': read_secret('MYSQL_ROOT_PASSWORD'),
+        'HOST': 'mariadb',
+        'PORT': '3306',
+    }
+}
